c67a8abe4421ea933f04d64a36887874980ce875
kishan-hungama <kishan.jariwala@hungama.com>
2017-06-27 16:18:08 +0530

    Added toast for 1st activity.

--- app/src/main/java/com/hungama/gitdemo/MainActivity.java
+++ app/src/main/java/com/hungama/gitdemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(), "Opened first activity.", Toast.LENGTH_SHORT).show();
 
         findViewById(R.id.button_test).setOnClickListener(new View.OnClickListener() {
             @Override
